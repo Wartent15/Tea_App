3a498d80c948526a284a234cd53a0cf6d3b2e74b
Wartent15 <163977218+Wartent15@users.noreply.github.com>
2025-06-19 01:22:27 +0300

    Добавьте файлы проекта.

---
+++ Tea_App/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Tea_App
+{
+    // не вижу смысла реализовать ту кучу кода что раньше.
+    // Так как есть textBox в которм можно самому ввести желаемый запрос.
+    // Если необходимо все же прописать запросы то отошлите работу обратно.
+    public partial class Form1 : Form
+    {
+        static string connectionString = @"Data Source=C:\Users\Artemik\source\repos\ConsoleApp13\Tea\database.db; Version=3";
+        static SQLiteConnection connection = new SQLiteConnection(connectionString);
+        DataSet set = null;
+        SQLiteDataAdapter da = null;
+        SQLiteCommandBuilder cmd = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }  
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string query_Tea = "CREATE TABLE Tea(" +
+                "Id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "NAME NVARCHAR," +
+                "Description NVARCHAR," +
+                "Amount INTEGER," +
+                "Price DECIMAL(10,2)," +
+                "CountryId INTEGER," +
+                "TypeId INTEGER," +
+                "FOREIGN KEY (CountryId)  REFERENCES Country (Id)," +
+                "FOREIGN KEY (TypeId)  REFERENCES Type (Id))";
+
+            string query_Country = "CREATE TABLE Country(" +
+                "Id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "NAME NVARCHAR)";
+
+            string query_Type = "CREATE TABLE Type(" +
+                "Id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "NAME NVARCHAR)";
+
+            SQLiteCommand command2 = new SQLiteCommand(query_Country, connection);
+            SQLiteCommand command3 = new SQLiteCommand(query_Type, connection);
+            SQLiteCommand command1 = new SQLiteCommand(query_Tea, connection);
+            connection.Open();
+            command2.ExecuteNonQuery();
+            command3.ExecuteNonQuery();
+            command1.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            set = new DataSet();
+            string query = textBox1.Text;
+            da = new SQLiteDataAdapter(query, connection);
+            dataGridView1.DataSource = null;
+            cmd = new SQLiteCommandBuilder(da);
+            da.Fill(set);
+            dataGridView1.DataSource = set.Tables[0];
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            da.Update(set);
+        }
+    }
+}
---
+++ Tea_App/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Tea_App")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tea_App")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2bb8a091-7eb4-4f28-a8fe-845beefd07ec")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
